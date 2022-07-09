c203183784bb569d2c0b6681db5fac73daa074a8
Dennis Okumu <dennisokumu72@yahoo.com>
2022-07-09 20:25:03 +0300

    Request API

--- app/src/main/java/com/example/score/MainActivity.kt
+++ app/src/main/java/com/example/score/MainActivity.kt
@@ -8,14 +8,19 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.score.ui.theme.ScoreTheme
+import com.example.score.viewmodel.InplayMatchesViewModel
+import com.example.score.viewmodel.state.MatchesState
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +29,28 @@ class MainActivity : ComponentActivity() {
                 Column(modifier = Modifier
                     .padding(10.dp)) {
                     TopAppBar()
+                    FetchData()
                 }
             }
         }
     }
 }
+@Composable
+fun FetchData(inplayMatchesViewModel: InplayMatchesViewModel = viewModel()) {
+    Column {
+     when(val state = inplayMatchesViewModel.inplayMatchesState.collectAsState().value){
+         is MatchesState.Empty -> Text(text = "No data available")
+         is MatchesState.Loading -> Text(text = "Loading...")
+         is MatchesState.Success -> Text(text = "Successful")
+         is MatchesState.Error -> Text(text = state.message)
+     }
+
+    }
+}
+
+
+
+
 @Composable
 fun TopAppBar() {
     Row(modifier = Modifier
